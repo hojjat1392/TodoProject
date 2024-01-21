91dcd05c35633a2c6c1691c91f046df90d020f32
hojjatAllah1371 <hojjat.rezapoor95@gmail.com>
2024-01-21 22:34:45 +0330

    create database todo model

--- model/todo.js
+++ model/todo.js
@@ -1,51 +1,22 @@
-const todoUtils = require("../utils/todos");
+const { DataTypes } = require("sequelize");
 
-class Todo {
-  constructor(id, text, completed = false) {
-    this.id = id;
-    this.text = text;
-    this.completed = completed;
-  }
+const sequelize = require("../utils/database");
 
-  save(callback) {
-    todoUtils.getTodos((todos) => {
-      todos.push(this);
-      todoUtils.saveTodos(todos, (err) => {
-        callback(err);
-      });
-    });
-  }
-
-  static fetchAll(callback) {
-    todoUtils.getTodos((todos) => {
-      callback(todos);
-    });
-  }
-
-  static deleteTodo(id, callback) {
-    todoUtils.getTodos((todos) => {
-      todoUtils.saveTodos(
-        todos.filter((t) => t.id != id),
-        (err) => {
-          callback(err);
-        }
-      );
-    });
-  }
-
-  static setTodoToComplete(id, callback) {
-    todoUtils.getTodos((todos) => {
-      const todoIndex = todos.findIndex((t) => t.id == id);
-
-      const todo = todos[todoIndex];
-      todo.completed = true;
-      todos[todoIndex] = todo;
-      
-      todoUtils.saveTodos(todos, (err) => {
-        callback(err);
-      });
-    });
-  }
-}
-
-module.exports = Todo;
+const Todo = sequelize.define("Todo", {
+  //model attributes:
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  text: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  completed: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+    allowNull: false,
+  },
+});
